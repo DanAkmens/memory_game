fbe865f8144a6c14ce3fc95f519f9d019e8c0382
Dainis Putans <dainisputans@Dainiss-MacBook-Pro.local>
2019-03-25 22:38:34 +0200

    Initial Commit

---
+++ Dainis Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dainis Says
+//
+//  Created by Dainis Putans on 25/03/2019.
+//  Copyright © 2019 Dainis Putans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
